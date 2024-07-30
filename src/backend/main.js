981b72dfd33eba5e1afe14a6b4f806f678e72167
mazhan <1007634428@qq.com>
2024-07-30 18:47:13 +0800

    init
    Signed-off-by: mazhan <1007634428@qq.com>

---
+++ src/backend/main.js
@@ -0,0 +1,94 @@
+import { app, BrowserWindow } from "electron";
+const path = require("node:path");
+import db from "./util/db";
+db.init();
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
+const createWindow = () => {
+  var desktop_wh = db.get("desktop_wh");
+  var desktop_wz = db.get("desktop_wz");
+console.log(desktop_wh, desktop_wz)
+  if (!desktop_wh) {
+    desktop_wh = "400,300";
+  }
+  if (!desktop_wz) {
+    desktop_wz = "356,429";
+  }
+  var arr_wh = desktop_wh.split(",");
+  var arr_wz = desktop_wz.split(",");
+
+  var width = parseInt(arr_wh[0]);
+  var height = parseInt(arr_wh[1]);
+
+  var x = parseInt(arr_wz[1]);
+  var y = parseInt(arr_wz[0]);
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    useContentSize: true,
+    width: width,
+    height: height,
+    y: x,
+    x: y,
+    maximizable: false,
+    minimizable: false,
+    transparent: true,
+    resizable: true,
+    frame: false,
+    hasShadow: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.setOpacity(1.0);
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
+  }
+
+  mainWindow.on("resize", () => {
+    var size = mainWindow.getSize();
+    db.set("desktop_wh", size[0].toString() + "," + size[1].toString());
+  });
+
+  mainWindow.on("move", () => {
+    var position = mainWindow.getPosition();
+    db.set("desktop_wz", position[0].toString() + "," + position[1].toString());
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  });
+};
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/backend/util/db.js
@@ -0,0 +1,59 @@
+"use strict";
+import { JSONFileSyncPreset } from "lowdb/node";
+import path from "path";
+import fs from "fs-extra";
+import { remote, app } from "electron";
+
+export default {
+  data() {
+    return {
+      db_util: null,
+      file_json: "",
+    };
+  },
+  async init() {
+    // if (process.env.NODE_ENV !== 'development') {
+    //     global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
+    // }
+
+    // if (process.env.DEBUG_ENV === 'debug') {
+    //     global.__static = path.join(__dirname, '../../static').replace(/\\/g, '\\\\')
+    // }
+
+    let APP = process.type === "renderer" ? remote.app : app;
+    let STORE_PATH = APP.getPath("userData");
+    // let STORE_PATH = "/Users/sanjin/work/h5/vue/thief-book/static"
+
+    if (process.type !== "renderer") {
+      if (!fs.pathExistsSync(STORE_PATH)) {
+        fs.mkdirpSync(STORE_PATH);
+      }
+    }
+
+    this.file_json = path.join(STORE_PATH, "/hidet_data.json");
+    this.db_util = JSONFileSyncPreset(this.file_json, { cfg: [] });
+  },
+  get(key) {
+    this.db_util.read();
+    const { cfg } = this.db_util.data;
+    const line = cfg.find((p) => (p.key = key));
+    if (line) {
+      return line.val;
+    }
+    return undefined;
+  },
+  set(key, value) {
+    this.db_util.update(({ cfg }) => {
+      let objToUpdate = cfg.find((obj) => obj.key === key);
+
+      if (objToUpdate) {
+        // 如果找到了对应的对象，则更新其 val 属性
+        objToUpdate.val = value;
+      } else {
+        // 如果没有找到对应的对象，则新增一个对象到数组中
+        cfg.push({ key: key, val: value });
+      }
+    });
+    this.db_util.write();
+  },
+};
